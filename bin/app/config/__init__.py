391839b2304d62dc3d8dd935dc7ec1049be8e87e
BOSCH luckas <boschluckas@gmail.com>
2023-09-05 22:21:33 +0200

    allow user to add / remove ip address

--- bin/app/config/__init__.py
+++ bin/app/config/__init__.py
@@ -1,6 +1,7 @@
 """Internal routing for config application"""
 from flask import Blueprint
-from .views import config_view
+from .views import config_view, delete_ip
 
 config = Blueprint('config', __name__, template_folder='templates', static_folder='static')
 config.add_url_rule('/', view_func=config_view, methods=['GET', 'POST'])
+config.add_url_rule('/delete', view_func=delete_ip, methods=['POST'])
