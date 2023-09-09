5e966ec1219ede7a83c57ee805fe73c5b4ade62c
BOSCH luckas <boschluckas@gmail.com>
2023-09-09 15:31:31 +0200

    allow user to manage users

--- bin/app/auth/__init__.py
+++ bin/app/auth/__init__.py
@@ -4,3 +4,7 @@ from .views import *
 
 auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 auth.add_url_rule('/login', view_func=login_view, methods=['GET', 'POST'])
+auth.add_url_rule('/users', view_func=users_view, methods=['GET'])
+auth.add_url_rule('/resetpasswd', view_func=resetpasswd_view, methods=['GET', 'POST'])
+auth.add_url_rule('/user/delete', view_func=delete_user_view, methods=['POST'])
+auth.add_url_rule('user/new', view_func=new_user_view, methods=['POST'])
--- bin/app/auth/views.py
+++ bin/app/auth/views.py
@@ -24,12 +24,63 @@ def login_view():
 
         if user and bcrypt.checkpw(password, user[2].encode('utf-8')):
             session['logged_in'] = True
-            print("OK CONNECTED")
             return redirect(url_for('home.index'))
             
         else:
-            print("!!!! NOT CONNECTED")
             return "Invalid credentials", 401
             
     else:
         return render_template('login.html')
+    
+
+def users_view():
+    conn = sqlite3.connect('app/database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT username FROM users")
+    users = cursor.fetchall()
+    conn.close()
+    user_list = [user[0] for user in users]
+    return render_template('users.html', users=user_list) 
+
+
+def resetpasswd_view():
+    if request.method == 'POST':
+        username = request.form['username']
+        new_password = request.form['new_password']
+        confirm_password = request.form['confirm_password']
+
+        if new_password == confirm_password:
+            hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+            conn = sqlite3.connect('app/database.db')
+            cursor = conn.cursor()
+            cursor.execute("UPDATE users SET password=? WHERE username=?", (hashed_password, username))
+            conn.commit()
+            conn.close()
+            return "Password updated successfully", 200
+        else:
+            return "Passwords do not match", 400
+    else:
+        return render_template('reset_password.html')
+
+def delete_user_view():
+    username = request.form['username']
+    conn = sqlite3.connect('app/database.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM users WHERE username=?", (username,))
+    conn.commit()
+    conn.close()
+    return "User deleted successfully", 200
+
+
+def new_user_view():
+    username = request.form['username']
+    password = request.form['password']
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    conn = sqlite3.connect('app/database.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+    conn.commit()
+    conn.close()
+    
+    return "User created successfully", 200
